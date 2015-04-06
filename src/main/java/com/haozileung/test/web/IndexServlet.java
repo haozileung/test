bef79f7b67bd1c50b833a108afb6be8926f2169b
YamchaL <yamchaleung@gmail.com>
2015-04-06 14:49:33 +0800

    new theme

--- src/main/java/com/haozileung/test/web/IndexServlet.java
+++ src/main/java/com/haozileung/test/web/IndexServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.context.Context;
-import org.apache.velocity.tools.view.VelocityViewServlet;
+import org.apache.velocity.tools.view.VelocityLayoutServlet;
 
 import com.haozileung.test.domain.security.User;
 import com.haozileung.test.infra.QueryHelper;
@@ -16,7 +16,7 @@ import com.haozileung.test.infra.cache.CacheHelper;
 import com.haozileung.test.infra.cache.ICacheInvoker;
 
 @WebServlet(name = "index", urlPatterns = { "/index" }, loadOnStartup = 1)
-public class IndexServlet extends VelocityViewServlet {
+public class IndexServlet extends VelocityLayoutServlet {
 
 	/**
      * 
