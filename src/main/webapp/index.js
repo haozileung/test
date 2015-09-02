c19b3bfa0a376742e6b6dfdaef1545bd1f6ed76d
HaoziLeung <yamchaleung@gmail.com>
2015-09-02 15:33:23 +0800

    update spm

---
+++ src/main/webapp/index.js
@@ -0,0 +1,4 @@
+require('./index.css');
+var $ = require('jquery');
+$('body').append('<h4>Hello World!</h4>');
+$("#btn").click(function(){alert("Hello World!");});
--- src/main/webapp/js/index.js
+++
@@ -1,4 +0,0 @@
-var $ = require('jquery');
-console.debug("000");
-$('body').append('<div>Hello World!</div>');
-console.debug("111");
