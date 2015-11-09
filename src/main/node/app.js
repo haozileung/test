3034f0bb32e5e3e8f4e0fc407b79ab0e994dd061
HaoziLeung <yamchaleung@gmail.com>
2015-11-09 14:36:17 +0800

    调整结构

--- src/main/node/app.js
+++ src/main/node/app.js
@@ -1,60 +1,55 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+app.set('view engine', 'ejs');
+app.use(logger('dev', {
+	skip : function(req, res) {
+		return res.statusCode < 400
+	}
+}));
+app
+		.get(
+				"/",
+				function(req, res) {
+					var down = 2;
+					res
+							.render(
+									'index',
+									function() {
+										setTimeout(
+												function() {
+													res
+															.write('<script>loaded("pagelet1", "I am pagelet 1!")</script>');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+													if (--down === 0) {
+														res
+																.end('</body></html>');
+													}
+												}, 4000);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+										setTimeout(
+												function() {
+													res
+															.write('<script>loaded("pagelet2", "I am pagelet 2!")</script>');
+													if (--down === 0) {
+														res
+																.end('</body></html>');
+													}
+												}, 3000);
+									});
+				});
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	console.error(err.stack);
+	var data = {
+		error : 'Something blew up!'
+	};
+	if (req.xhr) {
+		res.status(500).send(data);
+	} else {
+		res.status(500).send(data.error);
+	}
 });
-
-
+app.use(function(req, res, next) {
+	res.status(404).send('Sorry cant find that!');
+});
 module.exports = app;
