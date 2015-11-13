e41636261723970616952cb7915665e05da56e4d
HaoziLeung <yamchaleung@gmail.com>
2015-11-13 11:58:13 +0800

    koa test

--- src/main/node/app.js
+++ src/main/node/app.js
@@ -1,55 +1,19 @@
-var express = require('express');
-var logger = require('morgan');
-var app = express();
-app.set('view engine', 'ejs');
-app.use(logger('dev', {
-	skip : function(req, res) {
-		return res.statusCode < 400
-	}
-}));
-app
-		.get(
-				"/",
-				function(req, res) {
-					var down = 2;
-					res
-							.render(
-									'index',
-									function() {
-										setTimeout(
-												function() {
-													res
-															.write('<script>loaded("pagelet1", "I am pagelet 1!")</script>');
-
-													if (--down === 0) {
-														res
-																.end('</body></html>');
-													}
-												}, 4000);
-
-										setTimeout(
-												function() {
-													res
-															.write('<script>loaded("pagelet2", "I am pagelet 2!")</script>');
-													if (--down === 0) {
-														res
-																.end('</body></html>');
-													}
-												}, 3000);
-									});
-				});
-app.use(function(err, req, res, next) {
-	console.error(err.stack);
-	var data = {
-		error : 'Something blew up!'
-	};
-	if (req.xhr) {
-		res.status(500).send(data);
-	} else {
-		res.status(500).send(data.error);
-	}
+var compressor = require('koa-compressor');
+var koa = require('koa');
+var render = require('koa-ejs2x');
+var path = require('path');
+var app = koa();
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'template',
+  viewExt: 'html',
+  cache: true,
+  debug: false,
 });
-app.use(function(req, res, next) {
-	res.status(404).send('Sorry cant find that!');
+var index = require('./routes/index');
+app.use(compressor())
+app.use(index.routes());
+app.listen(3000, function() {
+	console.log('started on 3000');
 });
 module.exports = app;
--- src/main/node/routes/index.js
+++ src/main/node/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var router      = require('koa-router')();
+router.get("/",function *(next) {
+	yield this.render('index');
 });
-
+router.get("/login",function *(next) {
+    this.body = "xixi"
+});
 module.exports = router;
