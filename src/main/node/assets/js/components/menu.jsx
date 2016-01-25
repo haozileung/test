6a4f25280fd2861f3c23b7217153227087f2e25c
Yamcha Leung <yamchaleung@gmail.com>
2016-01-26 00:54:29 +0800

    react menu test

--- src/main/node/assets/js/components/menu.jsx
+++ src/main/node/assets/js/components/menu.jsx
@@ -2,15 +2,32 @@
 var React = require('react');
 var Menu  =  React.createClass ({  
   getInitialState: function() {
-    return {};
+    return {current:''};
+  },  
+  handlerClick: function(level,i){
+      if(level > 0){
+          this.setState({current:level});
+      }else{
+          this.setState({current:i});
+      }
   },  
   render :function(){
-   var menuList =   this.props.menus.map(function(menu,i){
+      var level = this.props.level;
+      var cx = require('classnames');
+      var classes = cx({
+          'nav': true,
+          'collapse': level > 0,
+          'in': level ===this.state.current
+      });
+      console.debug(level+"___"+this.state.current);
+      var click = this.handlerClick;
+      var _this = this;
+      var menuList = this.props.menus.map(function(menu,i){
                return Object.prototype.toString.call(menu.value) === '[object Array]'?
-               <li><a href="javascript:;">{menu.name}<span className="fa arrow"></span></a><Menu menus={menu.value} /></li>:
-               <li><a href={menu.value}>{menu.name}</a></li>;
+               <li key={i} onClick={click.bind(_this,level,i)}><a href="javascript:;">{menu.name}<span className="fa arrow"></span></a><Menu menus={menu.value} level={i} /></li>:
+               <li key={i} onClick={click.bind(_this,level,i)}><a href={menu.value}>{menu.name}</a></li>;
            });
-    return (<ul className="nav">{menuList}</ul>);
+    return (<ul className={classes}>{menuList}</ul>);
   }
 });
 module.exports = Menu;
