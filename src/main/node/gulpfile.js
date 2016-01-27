4654aeeafa8ba5fe21f907b1d86c29ee5860b266
Yamcha Leung <yamchaleung@gmail.com>
2016-01-27 23:31:35 +0800

    keep it simple

--- src/main/node/gulpfile.js
+++ src/main/node/gulpfile.js
@@ -1,110 +1,30 @@
 'use strict';
 var gulp = require('gulp');
-var gulpif = require('gulp-if');
-var watch = require('gulp-watch');
-var clean = require('gulp-rimraf');
-var jshint = require('gulp-jshint');
-var jshintjsx = require('jshint-jsx');
-var autoprefixer = require('gulp-autoprefixer');
-var minifycss = require('gulp-cssnano');
-var uglify = require('gulp-uglify');
-var connect = require('gulp-connect');
-var webpack = require('webpack-stream');
-var filter = require('gulp-filter');
-var source     = require('vinyl-source-stream'),
-    rename     = require('gulp-rename'),
-    browserify = require('browserify'),
-    glob       = require('glob'),
-    es         = require('event-stream');
-var jshintconfig = {
-	'node' : true,
-	'browser' : true,
-	'esnext' : true,
-	'bitwise' : true,
-	'camelcase' : true,
-	'curly' : true,
-	'eqeqeq' : true,
-	'immed' : true,
-	'indent' : 4,
-	'latedef' : true,
-	'newcap' : true,
-	'noarg' : true,
-	'quotmark' : 'single',
-	'regexp' : true,
-	'undef' : true,
-	'unused' : true,
-	'strict' : true,
-	'trailing' : true,
-	'smarttabs' : true,
-	'jquery' : true,
-	'linter' : require('jshint-jsx').JSXHINT
-};
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var webserver = require('gulp-webserver');
 
-gulp.task('build', function() {
-	glob('assets/js/*.js', function(err, files) {
-        if(err) {done(err);}
-        var tasks = files.map(function(entry) {
-            return browserify({ entries: [entry] })
-                .bundle()
-                .pipe(source(entry)).pipe(gulpif("production" === process.env.NODE_ENV, uglify()))
-                .pipe(rename({
-                    extname: '.bundle.js'
-                })).pipe(gulp.dest('public'));
-            });
-        es.merge(tasks).on('end', done);
+gulp.task('browserify', function() {
+  browserify('./app.jsx', { debug: true })
+    .transform(babelify)
+    .bundle()
+    .on("error", function (err) { console.log("Error : " + err.message); })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./*.jsx', ['browserify'])
+});
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      host: '127.0.0.1',
+      livereload: true
     })
-	return gulp.src('assets/**/*.js').pipe(jshint(jshintconfig)).pipe(
-			jshint.reporter('default')).pipe(webpack(config)).pipe(jsFilter)
-			
-			.pipe(jsFilter.restore).pipe(cssFilter).pipe(
-					autoprefixer('last 2 version')).pipe(
-					gulpif("production" === process.env.NODE_ENV, minifycss()))
-			.pipe(cssFilter.restore).pipe(gulp.dest('public'));
+  );
 });
 
-gulp.task('build-css', function() {
-    .pipe(
-					autoprefixer('last 2 version')).pipe(
-					gulpif("production" === process.env.NODE_ENV, minifycss()))
-			.pipe(cssFilter.restore).pipe(gulp.dest('public'));
-	glob('assets/less/*.less', function(err, files) {
-        if(err) {done(err);}
-        var tasks = files.map(function(entry) {
-            return browserify({ entries: [entry] })
-                .bundle()
-                .pipe(source(entry)).pipe(gulpif("production" === process.env.NODE_ENV, uglify()))
-                .pipe(rename({
-                    extname: '.bundle.js'
-                }))
-                .pipe(gulp.dest('./dist'));
-            });
-        es.merge(tasks).on('end', done);
-    })
-	return gulp.src('assets/**/*.js').pipe(jshint(jshintconfig)).pipe(
-			jshint.reporter('default')).pipe(webpack(config)).pipe(jsFilter)
-			
-			.pipe(jsFilter.restore).pipe(cssFilter).pipe(
-					autoprefixer('last 2 version')).pipe(
-					gulpif("production" === process.env.NODE_ENV, minifycss()))
-			.pipe(cssFilter.restore).pipe(gulp.dest('public'));
-});
-
-gulp.task('clean', function() {
-	return gulp.src([ './public' ], {
-		read : false
-	}).pipe(clean());
-});
-
-gulp.task('default', function() {
-	gulp.start('build');
-});
-gulp.task('connect', function() {
-	connect.server({
-		root : 'public',
-		livereload : true
-	});
-});
-gulp.task('watch', [ 'connect', 'build' ], function() {
-	gulp.watch([ './assets/**/*', './views/**/*.html' ], [ 'build' ]);
-	watch('./public/**/*').pipe(connect.reload());
-});
+gulp.task('default', ['browserify', 'watch', 'webserver']);
