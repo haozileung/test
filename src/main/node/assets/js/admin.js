8131996107d230af93587acf856012f1c6871f59
HaoziLeung <yamchaleung@gmail.com>
2016-02-04 12:03:55 +0800

    add bootstrap-datetimepicker

--- src/main/node/assets/js/admin.js
+++ src/main/node/assets/js/admin.js
@@ -3,7 +3,26 @@ require('./lib/common.js');
 function getFormData(params) {
     return $.extend(true, params, require('./lib/formutil.js').getData('testForm'));
 }
-$('#table').bootstrapTable({ url: 'data.json', sidePagination: 'server', pagination: true, queryParams: getFormData });
+$('#table').bootstrapTable({
+    sortOrder: 'desc',
+    url: 'data.json',
+    sidePagination: 'server',
+    pagination: true,
+    queryParams: getFormData,
+    columns: [
+        {
+            field: 'id',
+            title: 'Item ID'
+        },
+        {
+            field: 'name',
+            title: 'Item Name'
+        },
+        {
+            field: 'price',
+            title: 'Item Price'
+        }]
+});
 $("#test").on('click', function () {
     $('#table').bootstrapTable('selectPage', 1);
 });
